d8743dcb7d139d45fcf3221e404625b6941a59e4
Julia Paulasaari <julia@paulasaari.com>
2019-07-05 00:47:48 +0000

    Refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,28 @@
 const request = require('request');
 const breedName = process.argv[2];
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, reponse, body) {
-  // console.log('body:', body);
-  //console.log(typeof body);
+const fetchBreedDescription = function(breedName, callback) {
+
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, callback);
+  //   // console.log(body);
+  //   // console.log(typeof body);
+
+  //     if (error) {
+  //       return console.log('Failed to request details: ', error);
+  
+
   // const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  if (error) {
-    return console.log('Failed to request details: ', error);
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-});
+  //     // console.log(data);
 
+  // const breed = data[0];
+  //     if (breed) {
+  //       console.log(breed.description);
+  //     } else {
+  //       console.log(`Failed to find breed ${breedName}`);
+  //     }
+  //   });
 
-
+};
+module.exports = { fetchBreedDescription };
